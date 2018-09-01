ae103c43bf3336d7e76f97e7163a0ad9c93c24b3
StarBoy <upraveen@gmail.com>
2018-09-01 19:17:19 +0530

    server changes

--- config/Axios/axios.js
+++ config/Axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://192.168.1.40:3001"
-  // baseURL: 'https://wickedity.herokuapp.com',
+  //baseURL: "http://192.168.1.40:3001"
+  baseURL: "https://wickedity.herokuapp.com"
 });
